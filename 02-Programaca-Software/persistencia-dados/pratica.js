4ea4f6adbc443aadd85b7a51ff81d7d41a8945b0
astorius-br <art30hur@gmail.com>
2026-06-17 09:05:43 -0300

    Update pratica.js

--- 02-Programaca-Software/persistencia-dados/pratica.js
+++ 02-Programaca-Software/persistencia-dados/pratica.js
@@ -1,1 +1,12 @@
+// Persistência de dados usando o módulo fs
+// Abaixo o código de exemplo da apostila do prof. Jânio
+
+  const fs = require ( ' fs ') // importa o módulo fs
+  // abre o arquivo ' teste . txt ' e escreve ' Olá!' nele
+  
+  fs . writeFileSync ( ' teste . txt ' , " Olá! " )
+  // abre o arquivo ' teste . txt ' e acrescenta uma linha
+  
+  fs . appendFileSync ( ' teste . txt ' , " \ nMais uma linha . " )
+  // lê o conteúdo do arquivo ' teste . txt '
 
