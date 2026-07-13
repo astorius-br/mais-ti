6d05aa3e86e3cba2448b42544a914c6e6f159332
astorius-br <art30hur@gmail.com>
2026-07-13 11:03:48 -0300

    Create act.js

---
+++ 03-DEsenvolvimento-web/aula10/aula10-defato/act.js
@@ -0,0 +1,30 @@
+/*
+https://cssgradient.io/gradient-backgrounds/
+
+Para adicionar estilo gradiente em botão
+.btn-grad {
+  background-image: linear-gradient(to right, #757F9A 0%, #D7DDE8 51%, #757F9A 100%);
+}
+
+.btn-grad {
+  margin: 10px;
+  padding: 15px 45px;
+  text-align: center;
+  text-transform: uppercase;
+  transition: 0.5s;
+  background-size: 200% auto;
+  color: white;
+  box-shadow: 0 0 20 #eee;
+  border-radius: 10px;
+  display: block;
+  outline: none;
+  border: none;
+}
+
+.btn-grad:hover {
+  background-position: right center;
+  color: #fff;
+  text-decoration: none;
+}
+
+*/
