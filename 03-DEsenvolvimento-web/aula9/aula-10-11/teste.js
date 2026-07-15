a48e28df24fbd9b1eea88e90cb307bc610455246
astorius-br <art30hur@gmail.com>
2026-07-15 11:15:23 -0300

    Add files via upload

---
+++ 03-DEsenvolvimento-web/aula9/aula-10-11/teste.js
@@ -0,0 +1,32 @@
+const email = 'query@teste.com'
+verify = email.split("")
+let list = []
+let = verify
+
+let count = 0, ye = 0;
+verify.forEach(element => {
+    if (verify[element] == '@'){
+        count++; // conta quantidade de @
+        indexOf('@') //pegar o indice
+    }
+});
+
+if (count !== 1){
+    console.log('E-mail inválido')
+}
+
+verify = 
+
+console.log(verify, count, ye)
+
+
+/// string.split(separator, limit)
+// separator = @ para avaliar o conteúdo do e-mail
+// if forEach(email) não contém @ ou . após, retorne erro/inválido
+
+// catar cada elemento da string
+// verificar se há @
+//se não houver, email inválido
+// se houver, segue verificação
+   // verificar se há texto + . após @
+   // se não, email inválido
